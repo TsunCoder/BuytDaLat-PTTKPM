be57caafd3b5d057adaabd4ecb3ca2de43032f46
Thien Son <thiensoncva01@gmail.com>
2022-09-26 21:13:31 +0700

    connect database

---
+++ BusinessLayer/BaseObjectMethods/BusRouteFuns.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusRouteFuns
+    {
+        public List<BusRoute> Select_All()
+        {
+            using (var db = new ROUTE_MANAGEMENTEntities())
+            {
+                var ls = db.BusRoutes.AsQueryable();
+                if (ls != null && ls.Any())
+                    return ls.ToList();
+                return new List<BusRoute>();
+            }
+        }
+
+        public BusRoute Select_ID(int id)
+        {
+            using(var db = new ROUTE_MANAGEMENTEntities())
+            {
+                return db.BusRoutes.FirstOrDefault(s => s.BusRouteID == id);
+            }
+        }
+
+        public List<BusRoute> Select_IDs(List<string> IDs)
+        {
+            using(var db = new ROUTE_MANAGEMENTEntities())
+            {
+                var ls = db.BusRoutes.AsQueryable();
+                if (ls != null && ls.Any())
+                {
+                    ls = ls.Where(s => IDs.Contains(s.BusRouteID.ToString()));
+                    return ls.ToList();
+                }
+                return new List<BusRoute>();
+            }
+        }
+    }
+}
