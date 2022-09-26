0a67e2a1878975c50f480c57e61ed207834831ea
Thien Son <thiensoncva01@gmail.com>
2022-09-27 00:55:45 +0700

    Them san pham (Chua xong)

---
+++ BusinessLayer/System/BusRouteManagerSubsystem.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class BusRouteManagerSubsystem : BaseSystem
+    {
+        private static BusRouteManagerSubsystem instance = null;
+        
+        public static BusRouteManagerSubsystem Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new BusRouteManagerSubsystem();
+                return instance;
+            }
+        }
+    }
+}
--- BuytDaLat/Pages/Route.aspx.cs
+++ BuytDaLat/Pages/Route.aspx.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BusinessLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -12,6 +13,28 @@ namespace BuytDaLat.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private BusRoute GetValue()
+        {
+            BusRoute obj = new BusRoute();
+            obj.BusRouteID = this.txtID.Value.Length > 0 ? int.Parse(this.txtID.Value) : -1;
+            obj.RouteNumber = this.txtRouteNumber.Value;
+            obj.RouteName = this.txtRouteName.Value;
+            obj.ResponsibleUnitID = int.Parse(this.ddlResponsibleUnit.SelectedValue);
+            obj.OperationType = this.txtOperationType.Value;
+            obj.OperationTime = DateTime.Parse(this.txtOperationTime.Value);
+            obj.Fare = int.Parse(this.txtFare.Value);
+            obj.BusesAmount = int.Parse(this.txtBusesAmount.Value);
+            obj.BusesTime = DateTime.Parse(this.txtBusesTime.Value);
+            obj.BusesSpace = DateTime.Parse(this.txtBusesSpace.Value);
+            return obj;
+        }
+
+        protected void btLuu_Click(object sender, EventArgs e)
+        {
+            BusRoute obj = this.GetValue();
+            BusRouteManagerSubsystem.Instance.busRouteFuns.InsertUpdate(obj);
         }
     }
 }
